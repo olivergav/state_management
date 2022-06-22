c8562ec26c0dfac66f996068cce244a0693cbddb
olivergav <oliwier.gawlik0@gmail.com>
2022-06-22 22:48:05 +0200

    uuid, randomname installed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import Counter from "./components/Counter/Counter";
-import CounterFn from "./components/CounterFn/CounterFn";
+// import Counter from "./components/Counter/Counter";
+// import CounterFn from "./components/CounterFn/CounterFn";
 
 function App() {
-  return (
-    <div className="App">
-      {/*<Counter max={10}/>*/}
-      <CounterFn max={10}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<Counter max={10}/>*/}
+            {/*<CounterFn max={10}/>*/}
+
+
+        </div>
+    );
 }
 
 export default App;
